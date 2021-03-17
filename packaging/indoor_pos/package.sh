9e7c96b14fbe21992fc65fe787d710fca95ee2f9
Jari Nippula <jarix@ssrc.tii.ae>
2021-03-17 14:25:15 +0200

    indoor_pos packaging

---
+++ packaging/indoor_pos/package.sh
@@ -0,0 +1,26 @@
+build_nbr=$1
+
+echo "--- Generating /etc/ros/rosdep/sources.list.d/50-fogsw.list (as su)"
+mkdir -p /etc/ros/rosdep/sources.list.d
+
+echo "px4_msgs: \n    ubuntu: ros-foxy-px4-msgs \n" > rosdep.yaml
+echo "yaml file://${PWD}/rosdep.yaml" > /etc/ros/rosdep/sources.list.d/50-fogsw.list
+
+if [ ! -e /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+        echo "--- Initialize rosdep"
+        sudo rosdep init
+fi
+echo "--- Updating rosdep"
+rosdep update
+
+cd indoor_pos
+sed -i "s/[0-9]*<\/version>/${build_nbr}<\/version>/" package.xml
+version=$(grep "<version>" package.xml | sed 's/[^>]*>\([^<"]*\).*/\1/')
+title="$version ($(date +%Y-%m-%d))"
+dashes=$(printf '%*s' "${#title}" | tr ' ' "=")
+echo "$title" > CHANGELOG.rst
+echo "$dashes" >> CHANGELOG.rst
+echo "* commit: $(git rev-parse HEAD)" >> CHANGELOG.rst
+
+bloom-generate rosdebian --os-name ubuntu --os-version focal --ros-distro foxy && fakeroot debian/rules binary
+cd ..
