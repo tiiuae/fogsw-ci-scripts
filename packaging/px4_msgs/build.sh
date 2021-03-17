49940d5cadae3a5bce494d04a4ac08609dbf3e69
Jari Nippula <jarix@ssrc.tii.ae>
2021-03-17 14:24:50 +0200

    px4_msgs packaging

---
+++ packaging/px4_msgs/build.sh
@@ -0,0 +1,18 @@
+url=https://github.com/tiiuae/PX4-msgs.git
+name=px4_msgs
+
+if [ -d $name ]; then
+	rm -Rf $name
+fi
+
+git clone $url $name
+cd $name
+git submodule update --init --recursive
+cd ..
+docker build --build-arg BUILD_NUMBER=${GITHUB_RUN_NUMBER} -t fogsw-${name} .
+container_id=$(docker create fogsw-${name} "")
+docker cp ${container_id}:/packages .
+docker rm ${container_id}
+cp packages/*.deb ..
+rm -Rf packages
+rm -Rf $name
