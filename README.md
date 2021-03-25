b96b6d86e833b1ed30adeebc1bcf3a82ed7e33c6
Jari Nippula <jarix@ssrc.tii.ae>
2021-03-25 16:06:13 +0200

    Move ci scripts as separate directory
    CI/CD scripts are working in separate directory instead of being
    a submodule inside module to be built.
    
    Debian package output directory given as parameter

--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 Have GITHUB_RUN_NUMBER variable set in environmet the script is called
 
 ```
+git clone git@ghe.ssrc.fi:SSRC-EU/fogsw-ci-scripts.git
 git clone <module-url>
-cd <module>
-git clone git@ghe.ssrc.fi:SSRC-EU/fogsw-ci-scripts.git ./packaging/common
-./packaging/common/generate_deb.sh .
+cd fogsw-ci-scripts
+./generate_deb.sh <cloned-module-dir> <deb-output-dir>
+
 ```
 The debian package is generated into "module" directory
--- generate_deb.sh
+++ generate_deb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Usage: generate_deb.sh <module-dir> <deb-output-dir>
+
 if [ "$1" = "" ]; then
 	echo "ERROR: module root directory not given"
 	echo " usage: $0 <module root dir>"
@@ -8,17 +10,24 @@ fi
 
 build_dir=$(realpath $1)
 script_dir=$(dirname $(realpath $0))
+output_dir=$(realpath ${2:-${build_dir}})
+
 name=$(basename $build_dir)
+
+# Copy CI/CD scripts to module path
+mkdir -p ${build_dir}/packaging/common
+cp -f -R ${script_dir}/* ${build_dir}/packaging/common/
 
 # Copy mod_specific files if not available already
 if [ -d ${script_dir}/mod_specific/$name ]; then
-	cp -R ${script_dir}/mod_specific/$name/* ${build_dir}/packaging/
+	cp -f -R ${script_dir}/mod_specific/$name/* ${build_dir}/packaging/
 fi
 
+cd ${build_dir}
 # Prepare Dockerfile
-cp ${build_dir}/packaging/common/Dockerfile.template ./Dockerfile
+cp -f ${build_dir}/packaging/common/Dockerfile.template ./Dockerfile
 if [ -e ${build_dir}/packaging/Dockerfile.dep ]; then
-	sed -i '/^### INCLUDE_DEPENDENCIES/ r packaging/Dockerfile.dep' Dockerfile
+	sed -i '/^### INCLUDE_DEPENDENCIES/ r packaging/Dockerfile.dep' ./Dockerfile
 fi
 
 # Generate debian package
@@ -27,6 +36,7 @@ docker build --build-arg BUILD_NUMBER=${GITHUB_RUN_NUMBER} --build-arg DISTRIBUT
 container_id=$(docker create ${iname} "")
 docker cp ${container_id}:/packages .
 docker rm ${container_id}
-cp packages/*.deb .
+mkdir -p $output_dir
+cp packages/*.deb $output_dir
 rm -Rf packages
 
