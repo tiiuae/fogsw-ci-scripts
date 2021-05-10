54dc1191bcb53dfe9e6af78d76941eeccf7f8232
Manuel Segarra-Abad <manuel.segarra-abad@unikie.com>
2021-05-10 14:18:07 +0300

    Use hardened kernel sources

--- mod_specific/fogsw_kernel_config/build.sh
+++ mod_specific/fogsw_kernel_config/build.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-build_dir=$1
+BUILD_DIR=$1
+
+if [ -e linux ]; then
+    LINUX_SRC="/linux"
+elif [ -e linux-hwe-5.8-5.8.0 ]; then
+    LINUX_SRC="/linux-hwe-5.8-5.8.0"
+else
+    echo "ERROR: linux kernel sources are missing."
+    exit 1
+fi
 
 echo "$PWD Create kernel configuration."
-/${build_dir}/config/defconfig_builder.sh -k /${build_dir}/linux-hwe-5.8-5.8.0/ -t x86_kvm_release
-pushd /${build_dir}/linux-hwe-5.8-5.8.0/ > /dev/null
-make x86_kvm_release_defconfig
+# Other possible configurations:
+#    x86_kvm_release, x86_kvm_guest_release,
+#    x86_kvm_secure_release, x86_kvm_guest_secure_release
+/${BUILD_DIR}/config/defconfig_builder.sh -k /${BUILD_DIR}${LINUX_SRC} -t x86_debug
+pushd /${BUILD_DIR}${LINUX_SRC} > /dev/null
+make x86_debug_defconfig
 popd > /dev/null
 
 exit 0
--- mod_specific/fogsw_kernel_config/package.sh
+++ mod_specific/fogsw_kernel_config/package.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-build_dir=$1
+BUILD_DIR=$1
+
+if [ -e linux ]; then
+    LINUX_SRC="/linux"
+elif [ -e linux-hwe-5.8-5.8.0 ]; then
+    LINUX_SRC="/linux-hwe-5.8-5.8.0"
+else
+    echo "ERROR: linux kernel sources are missing."
+    exit 1
+fi
 
 echo "Build and package Linux Kernel."
-pushd /${build_dir}/linux-hwe-5.8-5.8.0/ > /dev/null
-fakeroot make-kpkg --initrd --revision=1.0.custom --jobs $(nproc) kernel_image
+pushd /${BUILD_DIR}${LINUX_SRC} > /dev/null
+fakeroot make-kpkg --initrd --revision=1 --jobs $(nproc) kernel_image
 mv ../*.deb /
 popd > /dev/null
 
