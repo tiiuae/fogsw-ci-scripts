f51a1a917b1dede6ee7e631d62f2606f5673a9b4
Jussi-Pekka Koro <jussi-pekka.koro@unikie.com>
2021-06-18 14:46:05 +0300

    DP-796: Dockerfile template for wpa. Depenpencies for wpa.

---
+++ generate_deb_wpa.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Usage: generate_deb.sh <module-dir> <deb-output-dir> <sub-path>
+
+set -e
+
+if [ "$1" = "" ]; then
+	echo "ERROR: module root directory not given"
+	echo " usage: $0 <module-dir> <deb-output-dir> <sub-path>"
+	exit 1
+fi
+
+build_base_dir=$(realpath $1)
+build_dir=$build_base_dir
+script_dir=$(dirname $(realpath $0))
+output_dir=$(realpath ${2:-${build_base_dir}})
+
+pushd ${build_base_dir}
+name=$(basename $(git config --get remote.origin.url) | sed 's/.git//')
+popd
+
+echo "build_base_dir=" $build_base_dir
+echo "build_dir=" $build_dir
+echo "script_dir=" $script_dir
+echo "output_dir=" $output_dir
+
+
+mod_specific_path=${script_dir}/mod_specific/${name}
+
+echo "mod_specific_path=" $mod_specific_path
+
+sub_path=${3}
+if [ "$sub_path" != "" ]; then
+	build_dir=$build_dir/$sub_path
+	mod_specific_path=$mod_specific_path/$sub_path
+fi
+
+echo "mod_specific_path=" $mod_specific_path
+
+# Copy CI/CD scripts to module path
+mkdir -p ${build_dir}/packaging/common
+cp -f -R ${script_dir}/* ${build_dir}/packaging/common/
+
+# Copy mod_specific files if not available already
+if [ -d $mod_specific_path ]; then
+	cp -f -R $mod_specific_path/* ${build_dir}/packaging/
+fi
+
+cd ${build_base_dir}
+echo "build_dir: " ${build_dir}
+# Prepare Dockerfile
+cp -f ${build_dir}/packaging/common/Dockerfile_wpa.template ./Dockerfile
+if [ -e ${build_dir}/packaging/Dockerfile_wpa.dep ]; then
+	echo "found .dep file !!!!!!!!!!!!!!!!!!111"
+	cp ${build_dir}/packaging/Dockerfile_wpa.dep .
+	sed -i '/^### INCLUDE_DEPENDENCIES/ r ./Dockerfile_wpa.dep' ./Dockerfile
+fi
+
+# Generate debian package
+iname=fogsw-${name,,}
+DEBIAN_FRONTEND=noninteractive
+docker build --no-cache --build-arg COMMIT_ID=$(git rev-parse HEAD) \
+	--build-arg GIT_VER=$(git log --date=format:%Y%m%d --pretty=~git%cd.%h -n 1) \
+	--build-arg BUILD_NUMBER=${GITHUB_RUN_NUMBER} \
+	--build-arg PACKAGE_SUBDIR=${sub_path} \
+	--build-arg DISTRIBUTION=${DISTRIBUTION} -t ${iname} .
+	
+container_id=$(docker create ${iname} "")
+docker cp ${container_id}:/packages .
+docker rm ${container_id}
+mkdir -p $output_dir
+cp packages/*.deb $output_dir
+rm -Rf packages
