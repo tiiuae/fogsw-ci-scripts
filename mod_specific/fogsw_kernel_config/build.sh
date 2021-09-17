b32696dd957a49d3b0f6882c2b7bbad13ef034fe
Manuel Segarra-Abad <manuel.segarra-abad@unikie.com>
2021-09-17 10:45:27 +0300

    Update kernel configuration for mesh
    Mesh needs to have some settings modified in the kernel
    configuration in order to have a working setup when there are not
    beacons of other networks in the area.

--- mod_specific/fogsw_kernel_config/build.sh
+++ mod_specific/fogsw_kernel_config/build.sh
@@ -10,6 +10,8 @@ if [ -e linux ]; then
     LINUX_SRC="/linux"
 elif [ -e linux-hwe-5.8-5.8.0 ]; then
     LINUX_SRC="/linux-hwe-5.8-5.8.0"
+elif [ -e linux-hwe-5.11-5.11.0 ]; then
+    LINUX_SRC="/linux-hwe-5.11-5.11.0"
 else
     echo "ERROR: linux kernel sources are missing."
     exit 1
@@ -22,7 +24,8 @@ fi
 
 if [ "${KERNEL_CONFIG}" != "x86_kvm_secure_release" ] && \
    [ "${KERNEL_CONFIG}" != "x86_kvm_guest_secure_release" ] && \
-   [ "${KERNEL_CONFIG}" != "x86_debug" ] ; then
+   [ "${KERNEL_CONFIG}" != "x86_debug" ] && \
+   [ "${KERNEL_CONFIG}" != "fog" ]; then
     echo "ERROR: linux kernel configuration (${KERNEL_CONFIG}) parameter is not valid."
     exit 1
 fi
@@ -31,9 +34,21 @@ echo "Create kernel configuration: ${KERNEL_CONFIG}."
 # Other possible configurations:
 #    x86_kvm_release, x86_kvm_guest_release,
 #    x86_kvm_secure_release, x86_kvm_guest_secure_release
-/${BUILD_DIR}/config/defconfig_builder.sh -k /${BUILD_DIR}${LINUX_SRC} -t ${KERNEL_CONFIG}
-pushd /${BUILD_DIR}${LINUX_SRC} > /dev/null
-make ${KERNEL_CONFIG}_defconfig
-popd > /dev/null
+#    ubuntu
+if [ "${KERNEL_CONFIG}" == "fog" ]; then
+    cp /${BUILD_DIR}/config/config-5.11.0-34-generic /${BUILD_DIR}${LINUX_SRC}/arch/x86/configs/${KERNEL_CONFIG}_defconfig
+    pushd /${BUILD_DIR}${LINUX_SRC} > /dev/null
+    make ${KERNEL_CONFIG}_defconfig
+    scripts/config --set-str SYSTEM_TRUSTED_KEYS ""
+    scripts/config --set-str SYSTEM_REVOCATION_KEYS ""
+    scripts/config --enable ATH_REG_DYNAMIC_USER_REG_HINTS
+    scripts/config --enable CFG80211_CERTIFICATION_ONUS
+    popd > /dev/null
+else
+    /${BUILD_DIR}/config/defconfig_builder.sh -k /${BUILD_DIR}${LINUX_SRC} -t ${KERNEL_CONFIG}
+    pushd /${BUILD_DIR}${LINUX_SRC} > /dev/null
+    make ${KERNEL_CONFIG}_defconfig
+    popd > /dev/null
+fi
 
 exit 0
